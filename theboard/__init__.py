56ec78e48cf61856460b2f3cbf19303d8fc031dd
Xiang Jaywhen <jaywhenxiang@foxmail.com>
2020-08-25 22:10:39 +0800

    add models

--- theboard/__init__.py
+++ theboard/__init__.py
@@ -1,3 +1,5 @@
+# 构造文件，包含程序实例
+# 启动程序时，首先执行的是该文件
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -5,4 +7,6 @@ app = Flask('theboard')
 app.config.from_pyfile('settings.py')
 
 db = SQLAlchemy(app)
+
+from theboard import errors, views, commands
 
